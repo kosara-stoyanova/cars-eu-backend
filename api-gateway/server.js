270072b968f8f496b6cc407068b4f9ab943d1cf6
Kosara Stoyanova <kosara.stoyanova@gmail.com>
2023-06-06 23:44:16 +0200

    updated workflow

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const gateway = require("express-gateway");
 const firebaseAdmin = require("firebase-admin");
+const serviceAccount = require("./.serviceAccounts/service-account.json");
 
-const serviceAccount = require("./.serviceAccounts/cars-eu-firebase-adminsdk-jm7e1-337727581c.json");
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(serviceAccount),
 });
